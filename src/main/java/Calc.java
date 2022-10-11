140df5afdaf41427e7d263046d945f00daff0582
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-11 13:42:30 -0400

    commit

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,5 +1,33 @@
 public class Calc {
-    public static int  add(String a){
-        return 0;
+    public static int add(String num) {
+
+        int result;
+        if (num.isEmpty()) {
+            return 0;
+        } else {
+            if(num.matches("\n")){
+                var splitNum1 = splitNum(num);
+                return pars(splitNum1);
+            }
+            var splitNum = splitNum(num);
+            result = pars(splitNum);
+        }
+        return result;
+    }
+
+    private static int pars(String[] split) {
+        int p = 0;
+        for (String s : split) {
+            p = Integer.parseInt(s) + p;
+        }
+        return p;
+    }
+
+    private static String[] splitNum(String num) {
+        return num.split(",|\n");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add("1,2\n3"));
     }
 }
