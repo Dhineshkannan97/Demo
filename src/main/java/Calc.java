c134ab6c28b112cf087b8c20b1547ada03f6e7e9
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-18 12:24:43 +0530

    stage 2

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,14 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calc {
     public static int add(String num) {
+//        if (num.replaceAll("[^A-za-z]","")){
+//
+//        }
 
         int result;
         if (num.isEmpty()) {
             return 0;
         } else {
-            if(num.matches("\n")){
+            if (num.contains(".")) {
+                var parseFloat = Float.parseFloat(num);
+                num.replaceAll(".","|");
+                System.out.println(parseFloat);
                 var splitNum1 = splitNum(num);
                 return pars(splitNum1);
             }
+
             var splitNum = splitNum(num);
             result = pars(splitNum);
         }
@@ -17,17 +27,29 @@ public class Calc {
 
     private static int pars(String[] split) {
         int p = 0;
+
         for (String s : split) {
+            Pattern pattern = Pattern.compile(".*[a-zA-Z]+.*");
+            Matcher matcher = pattern.matcher(s);
+//            String s1= String.valueOf(s.charAt(0));
+//            String s2=String.valueOf(s.charAt(1));
+            if (matcher.matches()) {
+//                s.replaceAll("",null);
+                continue;
+            }
             p = Integer.parseInt(s) + p;
         }
         return p;
     }
 
     private static String[] splitNum(String num) {
-        return num.split(",|\n");
+        var split = num.split(",");
+        return split;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(add("1,2\n3"));
+        System.out.println(add("9223372036854775807,5"));
+
     }
 }
--- src/main/java/org/example/Calc2.java
+++ src/main/java/org/example/Calc2.java
@@ -1,2 +1,48 @@
-package org.example;public class Calc2 {
+package org.example;
+
+public class Calc2 {
+    public static long add(String num) {
+
+
+        long result;
+        if (num.isEmpty()) {
+            return 0;
+        } else {
+
+
+//            var splitNum1 = splitNum(num);
+//            return pars(splitNum1);
+
+
+            var splitNum = splitNum(num);
+            result = pars(splitNum);
+
+            return result;
+        }
+    }
+
+    private static long pars(String[] split) {
+
+        long p = 0;
+        for (String s : split) {
+            //p = Long.parseLong(s) + p;
+            System.out.println(s);
+//            Pattern pattern = Pattern.compile(".*[a-zA-Z]+.*");
+//            Matcher matcher = pattern.matcher(s);matcher.matches()
+            if (Character.isAlphabetic(s.charAt(0))) {
+                continue;
+            }
+            p = Long.parseLong(s) + p;
+        }
+        return p;
+    }
+
+    private static String[] splitNum(String num) {
+        return num.split(",");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add("1,1111111111111111"));
+    }
+
 }
